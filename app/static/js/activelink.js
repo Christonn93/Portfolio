64ea743ad933befaede49279f1ff220c6c3e78dc
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-10-19 20:03:09 +0200

    Summary
    Added active state on links. Need more work because it don't get active on first load, but after a refresh it gets active.

--- app/static/js/activelink.js
+++ app/static/js/activelink.js
@@ -1,14 +1,26 @@
-function findActiveLink() {
-  const links = document.querySelector('a');
-  links.querySelector(checkIfLinkIsActive)
-}
+//*function findActiveLink() {
+//    const links = document.querySelectorAll('a href');
+//    links.forEach(checkIfLinkIsActive)
+//  }
+//  
+//  function checkIfLinkIsActive(link) {
+//    if (link.href.contains(location.pathname) ){
+//      link.classList.add('active');
+//    } else {
+//      link.classList.remove('active');
+//    }
+//    
+//  }
+//  window.addEventListener('DOMContentLoaded', findActiveLink)
 
-function checkIfLinkIsActive(link) {
-  if (link.href.contains(location.pathname)){
-    link.classList.add('active');
-  } else {
-    link.classList.remove('active');
-  }
-}
+window.onload = function () {
+    var all_links = document.getElementById("nav-link").getElementsByTagName("a"),
+    i=0, len=all_links.length,
+    full_path = location.href.split("#")[0];
 
-window.addEventListener('DOMContentLoaded', findActiveLink)
+    for(; i<len; i++){
+        if(all_links[i].href.split("#")[0] == full_path){
+            all_links[i].className = "nav-link-active";
+        }
+    }
+}

9444947b41592bb365c63e3cda9e1982cb79682f
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-10-20 11:25:45 +0200

    Summary
    Some more work on the content to the page

--- app/static/js/pages/home.js
+++ app/static/js/pages/home.js
@@ -8,23 +8,9 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>13 things you need to include in your career portfolio</h1>
-            <ul>
-            <li>1. CAREER SUMMARY</li>
-            <li>2. PHILOSOPHY STATEMENT</li>
-            <li>3. SHORT BIO</li>
-            <li>4. RESUME</li>
-            <li>5. SKILLS AND ABILITIES</li>
-            <li>6. ACCOMPLISHMENTS</li>
-            <li>7. WORK SAMPLES</li>
-            <li>8. HONORS</li>
-            <li>9. TRANSCRIPTS</li>
-            <li>10. PROFESSIONAL DEV</li>
-            <li>11. MILITARY RECORDS</li>
-            <li>12. VOLUNTEERING</li>
-            <li>13. REFERENCES</li>
-            </ul>
+            <h1>Welcome!</h1> 
 
+            <p>Here you will get to know me, get to know my skilles. If you have a project you want me to work on, donmt hassitate to contact me.</p> 
         `;
     }
 }
--- app/static/js/pages/mywork.js
+++ app/static/js/pages/mywork.js
@@ -8,8 +8,22 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Posts</h1>
-            <p>You are viewing the posts!</p>
+        <h1>13 things you need to include in your career portfolio</h1>
+        <ul>
+        <li>1. CAREER SUMMARY</li>
+        <li>2. PHILOSOPHY STATEMENT</li>
+        <li>3. SHORT BIO</li>
+        <li>4. RESUME</li>
+        <li>5. SKILLS AND ABILITIES</li>
+        <li>6. ACCOMPLISHMENTS</li>
+        <li>7. WORK SAMPLES</li>
+        <li>8. HONORS</li>
+        <li>9. TRANSCRIPTS</li>
+        <li>10. PROFESSIONAL DEV</li>
+        <li>11. MILITARY RECORDS</li>
+        <li>12. VOLUNTEERING</li>
+        <li>13. REFERENCES</li>
+        </ul>
         `;
     }
 }
